0878dc14ed510fceff518da27b1382dfabd8c3f5
baxino <baxino@gmail.com>
2024-01-19 13:14:38 +0100

    s

--- nagrody/projects/models.py
+++ nagrody/projects/models.py
@@ -5,3 +5,9 @@ class Project(models.Model):
     title= models.CharField(max_length=100)
     description=models.TextField()
     technology=models.CharField(max_length=20)
+    
+    
+
+    def __str__(self):
+        return self.title
+    
--- nagrody/projects/views.py
+++ nagrody/projects/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from projects.models import Project
+
+def project_index(request):
+    projects=Project.objects.all()
+    context={
+        "projects": projects
+    }
+    return render(request,"projects/project_index.html",context)
+
+
+    
